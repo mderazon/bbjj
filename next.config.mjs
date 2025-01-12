611a581507cf92a819957cd5b8c56ae1c2793ef7
Michael DeRazon <michaeld@scheduleonce.com>
2025-01-12 09:53:58 +0000

    Update domain

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
   images: {
     loader: "custom",
     loaderFile: "./lib/imageLoader.ts",
-    domains: ["www.bearbonesjiujitsu.com"],
+    domains: ["bearbonesjiujitsu.com"],
   },
   experimental: {
     webpackBuildWorker: true,
